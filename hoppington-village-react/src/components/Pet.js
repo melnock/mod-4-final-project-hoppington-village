2b327d6a7db2548a61c0386bc24bf3fc1b66ea97
github username <github email address>
2018-05-06 17:42:55 -0400

    weird logic-y magic

--- hoppington-village-react/src/components/Pet.js
+++ hoppington-village-react/src/components/Pet.js
@@ -1,11 +1,16 @@
 import React from 'react'
 
 const Pet = (props) => {
+  console.log(document.getElementById("angel"))
+
+  if (document.getElementById("angel")){
+  var rect = document.getElementById("angel").getBoundingClientRect();
+  console.log(rect.top, rect.right, rect.bottom, rect.left);}
 
   return (
     <div className="pet-image">
     {props.animal ?
-       <img onMouseEnter={props.handleMouseEnter} onMouseLeave={props.handleMouseLeave} src={props.animal.sprites.main} /> :
+       <img id={props.pet.name} onMouseEnter={props.handleMouseEnter} onMouseLeave={props.handleMouseLeave} src={props.animal.sprites.main} /> :
        null }
     </div>
   )
