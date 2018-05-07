40cb643151a521d52d2878890bac88db9229dd27
github username <github email address>
2018-05-07 13:37:06 -0400

    reversed meter colors

--- hoppington-village-react/src/components/PetGauges.js
+++ hoppington-village-react/src/components/PetGauges.js
@@ -7,9 +7,9 @@ const PetGauges = (props) => {
         <p>Energy:</p>
         <div className="energy-meter" style={{  borderLeft: (10-props.energy)*10 + "px solid red",   borderRight: (props.energy)*10 + "px solid green" }}></div>
         <br/><p>Cleanliness:</p>
-        <div className="energy-meter" style={{  borderLeft: (10-props.cleanliness)*10 + "px solid red",   borderRight: (props.cleanliness)*10 + "px solid green" }}></div>
+        <div className="energy-meter" style={{  borderLeft: (10-props.cleanliness)*10 + "px solid green",   borderRight: (props.cleanliness)*10 + "px solid red" }}></div>
         <br/><p>Hangry:</p>
-        <div className="energy-meter" style={{  borderLeft: (10-props.hangry)*10 + "px solid red",   borderRight: (props.hangry)*10 + "px solid green" }}></div>
+        <div className="energy-meter" style={{  borderLeft: (10-props.hangry)*10 + "px solid green",   borderRight: (props.hangry)*10 + "px solid red" }}></div>
       </div>
   )
   }
