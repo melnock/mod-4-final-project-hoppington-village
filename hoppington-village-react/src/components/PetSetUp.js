f77785f548547d7c9195b759f524b72270daa08f
github username <github email address>
2018-05-08 15:23:25 -0400

    added sleepy back

--- hoppington-village-react/src/components/PetSetUp.js
+++ hoppington-village-react/src/components/PetSetUp.js
@@ -15,12 +15,14 @@ class PetSetUp extends React.Component {
 
   render(){
     return (
-      <div>
+      <div className="form-stuff">
         <h1 className="pet-owner">Welcome to Hoppington Village, {this.props.auth.username}! </h1>
         <h2> It is time to choose your pet!</h2>
         <h2> What do you want to name your pet? </h2>
         <form onSubmit={(e)=>{this.props.handleBunnyCreation(e, this.state.name)}} >
-          <input name="name" value={this.state.name} onChange={this.handleChange}/>
+          <div className="text-field-component">
+            <input name="name" value={this.state.name} onChange={this.handleChange} placeholder="Name Your Pet"/>
+          </div>
           <br/><input type="submit"/>
         </form>
       </div>
